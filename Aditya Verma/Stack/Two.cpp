c9919d540601c33777a90b0de8fe727fe834962b
Reetesh Kumar <51451107+rishureetesh@users.noreply.github.com>
2021-03-24 00:10:09 +0530

    Final changes

--- Aditya Verma/Stack/Two.cpp
+++ Aditya Verma/Stack/Two.cpp
@@ -6,26 +6,28 @@
 using namespace std;
 int main(){
     int arr[]={3,1,5,2,8};
-    vector<int> returnArray;
+    stack<int> returnArray;
     stack<int> holdElements;
 
     for(int i=sizeof(arr)/sizeof(arr[0])-1;i>=0;i--){
         if(holdElements.size()==0){
-            returnArray.push_back(-1);
+            returnArray.push(-1);
         }else if(holdElements.size()>0 && holdElements.top()>arr[i]){
-            returnArray.push_back(holdElements.top());
+            returnArray.push(holdElements.top());
         }else if(holdElements.size()>0 && holdElements.top() <= arr[i]){
             while(holdElements.size() > 0 && holdElements.top() <= arr[i]){
                 holdElements.pop();
             }
             if(holdElements.size()==0){
-                returnArray.push_back(-1);
+                returnArray.push(-1);
             }else{
-                returnArray.push_back(holdElements.top());
+                returnArray.push(holdElements.top());
             }
         }
         holdElements.push(arr[i]);
     }
-    for(int item=returnArray.size()-1;item>=0;item--)
-        cout<<returnArray.at(item)<<endl;
+    while(returnArray.size()>0){
+        cout<<returnArray.top()<<" ";
+        returnArray.pop();
+    }
 }
