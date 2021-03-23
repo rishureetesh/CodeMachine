39d18339b48530e41cbb8ca535993877a1b6ab67
Reetesh Kumar <51451107+rishureetesh@users.noreply.github.com>
2021-03-23 23:19:59 +0530

    Code changes

---
+++ Aditya Verma/Stack/One.cpp
@@ -0,0 +1,11 @@
+//Nearest Greater to Left
+
+#include <iostream>
+using namespace std;
+int main(){
+    int arr={3,1,5,2,8};
+
+    for(int i=arr.length()-1;i>=0;i--){
+        for(int j=i;j<arr.length())
+    }
+}
