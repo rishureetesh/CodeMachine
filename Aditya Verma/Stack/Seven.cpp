56c32e48b8eccd8e6bf30ac60b583f09bb970495
Reetesh Kumar <51451107+rishureetesh@users.noreply.github.com>
2021-04-03 00:11:20 +0530

    Initial

---
+++ Aditya Verma/Stack/Seven.cpp
@@ -0,0 +1,13 @@
+// Max area of rectangle in binary matrix
+
+#include<iostream>
+
+int main(){
+    int data[]={
+        {0,1,1,0},
+        {1,1,1,1},
+        {1,1,1,1},
+        {1,1,0,0}
+    }
+    
+}
