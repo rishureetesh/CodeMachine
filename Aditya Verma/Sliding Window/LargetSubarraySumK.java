63096806682f8f7b08710b99335360a51ea396e0
Reetesh Kumar <51451107+rishureetesh@users.noreply.github.com>
2021-11-03 10:02:14 +0530

    Largest subarray with sum k, with ONLY +VE numbers

---
+++ Aditya Verma/Sliding Window/LargetSubarraySumK.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class LargetSubarraySumK{
+    public static void main(String [] args){
+        int[] Array = {9,2,1,4,6,2,4,3,-1,0,2};
+        int sumWindow = 12;
+        int start,end,sum;
+        start = end = sum = 0;
+        for(int i=0,j=0;j<Array.length;j++){
+            sum+=Array[j];
+            while(sum > sumWindow){
+                sum -= Array[i++];
+            }
+            if(sum == sumWindow && end-start+1 <= j-i+1){
+                start = i;
+                end = j;
+            }
+        }
+        System.out.println(start+"------------"+end+" Size of subarray : "+(end-start+1));
+    }
+}
