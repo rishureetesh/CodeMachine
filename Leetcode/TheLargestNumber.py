47beaf6162ed5534c4eb790843beeb5a5d313b3b
Reetesh Kumar <51451107+rishureetesh@users.noreply.github.com>
2022-10-16 13:33:34 +0530

    Code refactoring

--- Leetcode/TheLargestNumber.py
+++ Leetcode/TheLargestNumber.py
@@ -1,23 +1,15 @@
 def findLargestNumber(itemList, money):
+    lst = [0 for j in range(len(itemList)+1)]
     matrix = [[0 for j in range(len(itemList)+1)] for i in range(money+1)]
     for i in range(1, len(matrix)):
         for j in range(1, len(matrix[0])):
             if itemList[j-1] <= money - matrix[i-1][j]:
                 matrix[i][j] = matrix[i-1][j] + 1
+                lst[j] = lst[j] * 10 + j
             else:
                 matrix[i][j] = matrix[i-1][j]
-    max = matrix[money][0]
-    ind = 0
-    for index in range(1, len(matrix[money])):
-        if matrix[money][index] >= max:
-            max = matrix[money][index]
-            ind = index
-    if max == 0:
-        return -1
-    dataReturn = 0
-    for i in range(max):
-        dataReturn = dataReturn * 10 + ind
-    return dataReturn
+    print(lst)
+    return max(lst)
     
 
 
